fca5f434913b043350ca1b635079697fd9c941ea
Dino Chang <kenlion961@hotmail.com>
2017-06-01 03:24:43 +0800

    91 大 code review 後的調整

--- HomeworkDay1/HomeworkDay1/CalculateSum.cs
+++ HomeworkDay1/HomeworkDay1/CalculateSum.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,103 +9,54 @@ namespace HomeworkDay1
 {
     public class CalculateSum
     {
-        private List<Product> GetProducts()
+        private List<Product> _products;
+
+        public CalculateSum(List<Product> products)
         {
-            var products = new List<Product>
-            {
-                new Product{ ID =  1, Cost =  1, Revenue = 11, SellPrice = 21},
-                new Product{ ID =  2, Cost =  2, Revenue = 12, SellPrice = 22},
-                new Product{ ID =  3, Cost =  3, Revenue = 13, SellPrice = 23},
-                new Product{ ID =  4, Cost =  4, Revenue = 14, SellPrice = 24},
-                new Product{ ID =  5, Cost =  5, Revenue = 15, SellPrice = 25},
-                new Product{ ID =  6, Cost =  6, Revenue = 16, SellPrice = 26},
-                new Product{ ID =  7, Cost =  7, Revenue = 17, SellPrice = 27},
-                new Product{ ID =  8, Cost =  8, Revenue = 18, SellPrice = 28},
-                new Product{ ID =  9, Cost =  9, Revenue = 19, SellPrice = 29},
-                new Product{ ID = 10, Cost = 10, Revenue = 20, SellPrice = 30},
-                new Product{ ID = 12, Cost = 11, Revenue = 21, SellPrice = 31}
-            };
-
-
-            return products;
-        }
-
-        public List<int> GetCostSumBy3Count()
-        {
-            return GetCostSumByGroupCount(3);
-        }
-
-        public List<int> GetRevenueSumBy4Count()
-        {
-            return GetRevenueSumByGroupCount(4);
+            this._products = products;
         }
 
 
         public List<int> GetCostSumByGroupCount(int count)
         {
-            var result = new List<int>();
+            Expression<Func<Product, int>> sumPredicate = r => r.Cost;
 
-            var products = GetProducts();
-
-            if (products.Any() == false)
-            {
-                result.Add(0);
-                return result;
-            }
-
-            var step = (int)Math.Ceiling(Convert.ToDouble(products.Count) / count);
-
-            products = products.OrderBy(r => r.ID).ToList();
-
-            for (int i = 0; i < step; i++)
-            {
-                var groupSum = products
-                    .Skip(i * count)
-                    .Take(count)
-                    .Sum(r => r.Cost);
-
-                result.Add(groupSum);
-            }
-
-            return result;
+            return GetSumResultByCount<Product>(_products, count, sumPredicate);
 
         }
 
         public List<int> GetRevenueSumByGroupCount(int count)
         {
+            Expression<Func<Product, int>> sumPredicate = r => r.Revenue;
+
+            return GetSumResultByCount<Product>(_products, count, sumPredicate);
+
+        }
+
+        private List<int> GetSumResultByCount<T>(List<T> data, int count, Expression<Func<T,int>> predicate)
+        {
             var result = new List<int>();
 
-            var products = GetProducts();
-
-            if (products.Any() == false)
-            {
-                result.Add(0);
-                return result;
-            }
-
-            var step = (int)Math.Ceiling(Convert.ToDouble(products.Count) / count);
-
-            products = products.OrderBy(r => r.ID).ToList();
+            var step = (int)Math.Ceiling(Convert.ToDouble(data.Count) / count);
 
             for (int i = 0; i < step; i++)
             {
-                var groupSum = products
+                var groupSum = data.AsQueryable()
                     .Skip(i * count)
                     .Take(count)
-                    .Sum(r => r.Revenue);
+                    .Sum(predicate);
 
                 result.Add(groupSum);
             }
 
             return result;
-
         }
 
         public int GetCostSumByIdGroup(List<int> ids)
         {
             var result = new List<int>();
 
-            var products = GetProducts()
+            var products = _products
                 .Where(r => ids.Contains(r.ID));
 
             return products.Sum(r => r.Cost);
@@ -114,10 +66,11 @@ namespace HomeworkDay1
         {
             var result = new List<int>();
 
-            var products = GetProducts()
+            var products = _products
                 .Where(r => ids.Contains(r.ID));
 
             return products.Sum(r => r.Revenue);
         }
+
     }
 }
--- HomeworkDay1/HomeworkDay1/UnitTest_CalculateSum.cs
+++ HomeworkDay1/HomeworkDay1/UnitTest_CalculateSum.cs
@@ -8,38 +8,32 @@ namespace HomeworkDay1
     [TestClass]
     public class UnitTest_CalculateSum
     {
-        [TestMethod]
-        public void 三筆一組_CostSum()
+        private List<Product> _products;
+
+        [TestInitialize]
+        public void Init()
         {
-            var target = new CalculateSum();
-
-            var expected = new List<int> { 6, 15, 24, 21 };
-
-            var actual = target.GetCostSumBy3Count();
-
-            expected.ToExpectedObject().ShouldEqual(actual);
-
-        }
-
-
-        [TestMethod]
-        public void 四筆一組_RevenueSum()
-        {
-            var target = new CalculateSum();
-
-            var expected = new List<int> { 50, 66, 60 };
-
-            var actual = target.GetRevenueSumBy4Count();
-
-            expected.ToExpectedObject().ShouldEqual(actual);
-
+            this._products = new List<Product>
+            {
+                new Product{ ID =  1, Cost =  1, Revenue = 11, SellPrice = 21},
+                new Product{ ID =  2, Cost =  2, Revenue = 12, SellPrice = 22},
+                new Product{ ID =  3, Cost =  3, Revenue = 13, SellPrice = 23},
+                new Product{ ID =  4, Cost =  4, Revenue = 14, SellPrice = 24},
+                new Product{ ID =  5, Cost =  5, Revenue = 15, SellPrice = 25},
+                new Product{ ID =  6, Cost =  6, Revenue = 16, SellPrice = 26},
+                new Product{ ID =  7, Cost =  7, Revenue = 17, SellPrice = 27},
+                new Product{ ID =  8, Cost =  8, Revenue = 18, SellPrice = 28},
+                new Product{ ID =  9, Cost =  9, Revenue = 19, SellPrice = 29},
+                new Product{ ID = 10, Cost = 10, Revenue = 20, SellPrice = 30},
+                new Product{ ID = 12, Cost = 11, Revenue = 21, SellPrice = 31}
+            };
         }
 
         [TestMethod]
 
-        public void 指定三筆一組_CostSum()
+        public void 測試_指定三筆一組_Cost總和_依循應為_6_15_24_21()
         {
-            var target = new CalculateSum();
+            var target = new CalculateSum(_products);
 
             var expected = new List<int> { 6, 15, 24, 21 };
 
@@ -51,9 +45,9 @@ namespace HomeworkDay1
 
         [TestMethod]
 
-        public void 指定四筆一組_RevenueSum()
+        public void 測試_指定四筆一組_Revenue總和_依循應為_50_66_60()
         {
-            var target = new CalculateSum();
+            var target = new CalculateSum(_products);
 
             var expected = new List<int> { 50, 66, 60 };
 
@@ -65,9 +59,9 @@ namespace HomeworkDay1
 
         [TestMethod]
 
-        public void 指定126為一組_CostSum()
+        public void 測試_指定ID_126為一組_Cost總和_應為9()
         {
-            var target = new CalculateSum();
+            var target = new CalculateSum(_products);
 
             var expected = 9;
 
@@ -80,9 +74,9 @@ namespace HomeworkDay1
         }
 
         [TestMethod]
-        public void 指定347為一組_RevenueSum()
+        public void 測試_指定ID_347為一組_Revenue總和_應為44()
         {
-            var target = new CalculateSum();
+            var target = new CalculateSum(_products);
 
             var expected = 44;
 
@@ -92,6 +86,19 @@ namespace HomeworkDay1
 
             Assert.AreEqual(expected, actual);
 
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void 測試_Products_為NULL_應該丟出_NullReferenceException()
+        {
+            var target = new CalculateSum(null);
+
+            var expected = new List<int> { 50, 66, 60 };
+
+            var actual = target.GetRevenueSumByGroupCount(4);
+
+            expected.ToExpectedObject().ShouldEqual(actual);
         }
     }
 }
